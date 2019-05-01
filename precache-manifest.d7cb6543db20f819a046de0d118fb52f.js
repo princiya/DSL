48b59089df7cabb3fd9d746aacbfa2b0a7fd0ece
Princiya <princiya.marina@gmail.com>
2019-05-01 23:52:00 +0200

    first commit

---
+++ precache-manifest.d7cb6543db20f819a046de0d118fb52f.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "main.10125d0000e9b401c80e.bundle.js"
+  },
+  {
+    "url": "runtime~main.10125d0000e9b401c80e.bundle.js"
+  },
+  {
+    "url": "vendors~main.10125d0000e9b401c80e.bundle.js"
+  },
+  {
+    "revision": "c6eb469a59c4f238fc7f0a424ecade79",
+    "url": "iframe.html"
+  }
+];
